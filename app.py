0510a9fc92ac6317a7c602fb2054c72e3e4e0340
skgnt <shinn14h.m0304@gmail.com>
2025-01-12 11:27:26 +0900

    toshokan.csvを読み込み、年度ごとのデータをフィルタリングして線グラフを表示する機能を追加

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
 import streamlit as st
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+
+@st.cache_data
+def load_data():
+    return pd.read_csv('toshokan.csv')
+
+#wide表示
+st.set_page_config(layout="wide")
+
+#toshokan.csvをheaderありで読み込む
+
+df = load_data()
+
+#dfのyearの最小値と最大値を取得
+min_year = df['year'].min()
+max_year = df['year'].max()
+
+#スライダーを表示
+start_year, end_year = st.slider('年度', min_value=min_year, max_value=max_year, value=(min_year, max_year))
+
+#yearでフィルタリング
+df = df[(df['year'] >= start_year) & (df['year'] <= end_year)]
+#年度ごとの件数を集計
+df_grouped = df.groupby('year').size().reset_index(name='count')
+#線グラフを表示
+st.line_chart( df_grouped.set_index('year') )
+#1.categoryごとの件数を集計
+#2.年を検索できるようにして、その年のデータを表で表示
+#3.年ごとではなく、月ごとに集計して、棒グラフで表示
